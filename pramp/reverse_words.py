d147d56b41f21fd7b7a4e9393045bc9c7396c7a3
tremlab <tremlab@icloud.com>
2017-05-22 02:37:41 +0000

    pramp  challenge - reverse whole words in list.

---
+++ pramp/reverse_words.py
@@ -0,0 +1,29 @@
+def reverseWords(arr):
+    words = []
+    word = []
+
+    for char in arr:
+
+      if char == "  ":
+          words.append(word)
+          word = []
+      else:
+          word.append(char)
+
+
+    words.append(word)
+
+    words.reverse()
+
+    reversed_list = []
+
+    for word in words:
+        word.append("  ")
+        reversed_list.extend(word)
+
+    reversed_list.pop()
+
+    return reversed_list
+
+
+print reverseWords([ 'c', 'a', 't', '  ', 'f', 'a', 't'])
