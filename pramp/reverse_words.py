5f320dd0d64671df2a1bff15d77bd589219e4028
tremlab <tremlab@icloud.com>
2017-06-04 16:38:05 +0000

    time/space efficient solution.

--- pramp/reverse_words.py
+++ pramp/reverse_words.py
@@ -1,3 +1,30 @@
+def rev_words_efficient(arr):
+    arr.reverse()
+
+    start = 0
+
+    for i in range(len(arr)):
+        if arr[i] == "  ":
+            stop = i - 1
+            mirror(start, stop, arr)
+            start = i + 1
+
+    mirror(start, len(arr) - 1, arr)
+
+    return arr
+
+
+def mirror(start, stop, arr):
+    while start < stop:
+
+        arr[start], arr[stop] = arr[stop], arr[start]
+        start += 1
+        stop -= 1
+
+
+
+
+
 def reverseWords(arr):
     words = []
     word = []
@@ -25,4 +52,5 @@ def reverseWords(arr):
     return reversed_list
 
 
-print reverseWords([ 'c', 'a', 't', '  ', 'f', 'a', 't'])
+print reverseWords(['c', 'a', 't', '  ', 'f', 'a', 't'])
+print rev_words_efficient(['c', 'a', 't', '  ', 'f', 'a', 't', 't', 'y'])
